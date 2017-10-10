8105624af984888737ef1ec070775fb8554e7f96
EvaMi <milova_evg@mail.ru>
2017-10-10 23:50:33 +0300

    Done Task2

--- src/Task2.java
+++ src/Task2.java
@@ -6,16 +6,17 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         try {
+            int sum;
             //консольный ввод данных
             Scanner in = new Scanner(System.in);
             System.out.println("Enter n: ");
             int n = in.nextInt();
-            if (n%100 == n){
-                int sum = (n / 10) + (n % 10);
+            if ((n > 0) && (n % 100 == n) && (n % 10 != n)){
+                sum = (n / 10) + (n % 10);
                 System.out.println("Sum: " + sum);
             }
             else{
-                System.out.println(two-digit number);
+                System.out.println("not two-digit number");
             }
         }
         catch(Exception e){
