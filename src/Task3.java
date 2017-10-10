fe87ce5d22dcf6a583e830f3187a13ecf9e66f33
EvaMi <milova_evg@mail.ru>
2017-10-11 00:34:12 +0300

    Выполнено 4 задание полностью Задание 3 в разработке

---
+++ src/Task3.java
@@ -0,0 +1,18 @@
+/**
+ * Created by evami on 10.10.17.
+ */
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        try{
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter n: ");
+            double n = in.nextDouble();
+
+        }
+        catch(Exception e){
+            System.out.println("Input error ");
+        }
+    }
+}
