a12db4045236fa89a112fa06afd36f69a64a48dc
EvaMi <milova_evg@mail.ru>
2017-12-11 20:16:28 +0300

    где то здесь должен быть bank версия 3) оно живоооое

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,6 +1,4 @@
 package bank;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 
@@ -14,14 +12,15 @@ public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
         Bank bank = new Bank();
-        ExecutorService poolMailers = Executors.newFixedThreadPool(10);
+        Mailer mailer = new Mailer();
+        mailer.start();
         ExecutorService poolTasks = Executors.newFixedThreadPool(4);
         for (int i = 0; i < 100; i++)
             bank.addUser(new Bank.User("User " + i));
         for (int i = 0; i < 100; i++)
             bank.addAccount(new Bank.Account(i + 1000, rnd.nextInt(bank.getUsers().size())));
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
              poolTasks.submit(new Runnable() {
                 @Override
                 public void run() {
@@ -35,7 +34,6 @@ public class Main {
                     TxResult result = bank.transferMoney(src, dest, amount);
 
                     mailerTasksDeque.add(new Transaction(src,dest, amount, result));
-
                 }
             });
         }
@@ -44,28 +42,29 @@ public class Main {
 
     private static class Mailer extends Thread {
         @Override
-        public void run() {{
-                try {
-                    Transaction trans = mailerTasksDeque.takeFirst();
-                    System.out.println();
-                    if (trans.result == TxResult.SUCCESS){
-                        String message = "Списание с " + trans.src.getId() + " на сумму " + trans.amount + "\n"
-                                + "Баланс = " + trans.src.getBalance() + "\n"
-                                + "Зачисление на " + trans.dest.getId() + " " + trans.amount;
-                        System.out.println(message);
+        public void run() {
+                while (!isInterrupted()) {
+                    try {
+                        Transaction trans = mailerTasksDeque.takeFirst();
+                        System.out.println();
+                        if (trans.result == TxResult.SUCCESS) {
+                            String message = "Списание с " + trans.src.getId() + " на сумму " + trans.amount + "\n"
+                                    + "Баланс = " + trans.src.getBalance() + "\n"
+                                    + "Зачисление на " + trans.dest.getId() + " " + trans.amount;
+                            System.out.println(message);
+                        } else if (trans.result == TxResult.NOT_ENOUGH) {
+                            System.out.println("Недостаточно средств на " + trans.src.getId());
+                        } else if (trans.result == TxResult.SAME_ACCOUNT) {
+                            System.out.println("Перевод на тот же аккаунт невозможен " + trans.src.getId() + " " + trans.dest.getId());
+                        }
+                    } catch (InterruptedException e) {
+                        interrupt();
+                        e.printStackTrace();
                     }
-                    else if (trans.result == TxResult.NOT_ENOUGH){
-                        System.out.println("Недостаточно средств на " + trans.src.getId());
-                    }
-                    else if (trans.result == TxResult.SAME_ACCOUNT){
-                        System.out.println("Перевод на тот же аккаунт невозможен " + trans.src.getId() + " " + trans.dest.getId());
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
             }
         }
-    }
+
 
     private static class Transaction{
         private Bank.Account src;
